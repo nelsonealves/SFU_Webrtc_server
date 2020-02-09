31049e49e2b5b743b7db7237e77612b49aec6776
Nelson <nelsonespindolalves@gmail.com>
2020-02-08 23:25:38 -0200

    adicionar server e front

--- controllers/worker.js
+++
@@ -1,5 +0,0 @@
-let mediasoup = require('../mediasoup');
-module.exports.create_worker = async function (req, res) {
-    console.log(mediasoup);
-    let worker = await mediasoup.createWorker({logLevel: "warn"});
-}
